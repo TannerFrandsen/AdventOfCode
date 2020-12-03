cdeb9354099ba3ac4a2e1773514e98696c4c9760
Tanner Frandsen <tandaman29@yahoo.com>
2020-12-03 16:21:52 -0700

    Adding a recursive solution to search for a sum of n numbers

--- solutions/day_01.py
+++ solutions/day_01.py
@@ -1,5 +1,7 @@
 # day_01
 # Tanner Frandsen
+import copy
+
 
 def load_puzzle_data(file):
     with open(file, 'r') as file_handle:
@@ -28,8 +30,27 @@ def find_three_numbers_sum_to_target(puzzle_input, target):
                 return first_num, second_num, complement
 
 
+def find_n_numbers_sum_to_target(target, puzzle_input, n, current_sum=0):
+    # this could be cleaned up to return the values insted of printing them
+    if current_sum == target and n == 0:
+        return True
+    if n == 0:
+        return False
+
+    for x in puzzle_input:
+        current_sum = current_sum + x
+        filtered_list = copy.copy(puzzle_input)
+        filtered_list.remove(x)
+        if find_n_numbers_sum_to_target(target, filtered_list, n-1, current_sum):
+            print(x)
+            return True
+        else:
+            current_sum = current_sum - x
+
+
 def main(input_path, target):
     puzzle_input = load_puzzle_data(input_path)
+
     num1, num2 = find_two_numbers_sum_to_target(puzzle_input, target)
     print(f'{num1} + {num2} = {num1 + num2}')
     print(f'{num1} * {num2} = {num1 * num2}')
@@ -37,6 +58,13 @@ def main(input_path, target):
     num1, num2, num3 = find_three_numbers_sum_to_target(puzzle_input, target)
     print(f'{num1} + {num2} + {num3}= {num1 + num2 + num3}')
     print(f'{num1} * {num2} * {num3}= {num1 * num2 * num3}')
+
+    print('Recursive solution that supports n numbers summed to target')
+    find_n_numbers_sum_to_target(2020, puzzle_input, 2)
+
+    print('')
+    find_n_numbers_sum_to_target(2020, puzzle_input, 3)
+    print('')
 
 
 if __name__ == '__main__':
