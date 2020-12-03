407c32e41dcdc894466c3737fc3b084923d2b79a
Tanner Frandsen <tandaman29@yahoo.com>
2020-12-03 13:52:52 -0700

    Convert puzzle data to set to optimize lookup speed. Adding solution for puzzle 2 to find three numbers which sum to target.

--- solutions/day_01.py
+++ solutions/day_01.py
@@ -9,19 +9,34 @@ def load_puzzle_data(file):
     return [int(number) for number in data]
 
 
-def find_numbers_sum_to_target(puzzle_input, target):
-    # Find first numbers which sum to target
-    for number in puzzle_input:
+def find_two_numbers_sum_to_target(puzzle_input, target):
+    puzzle_data = set(puzzle_input)
+    # Find first pair of numbers which sum to target
+    for number in puzzle_data:
         complement = target - number
         if complement in puzzle_input:
             return number, complement
 
 
+def find_three_numbers_sum_to_target(puzzle_input, target):
+    puzzle_data = set(puzzle_input)
+    # Find first set of three of numbers which sum to target
+    for first_num in puzzle_data:
+        for second_num in puzzle_data:
+            complement = target - (first_num + second_num)
+            if complement in puzzle_data:
+                return first_num, second_num, complement
+
+
 def main(input_path, target):
     puzzle_input = load_puzzle_data(input_path)
-    num1, num2 = find_numbers_sum_to_target(puzzle_input, target)
+    num1, num2 = find_two_numbers_sum_to_target(puzzle_input, target)
     print(f'{num1} + {num2} = {num1 + num2}')
     print(f'{num1} * {num2} = {num1 * num2}')
+
+    num1, num2, num3 = find_three_numbers_sum_to_target(puzzle_input, target)
+    print(f'{num1} + {num2} + {num3}= {num1 + num2 + num3}')
+    print(f'{num1} * {num2} * {num3}= {num1 * num2 * num3}')
 
 
 if __name__ == '__main__':
